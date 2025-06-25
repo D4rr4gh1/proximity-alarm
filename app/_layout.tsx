00c566a915289c6fc7e8399ac331b2330333cba0
Darragh Connolly <darraghconnolly03@hotmail.com>
2025-06-25 15:44:47 +0100

    Location services added. Draggable pin with state coords added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,68 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as Location from 'expo-location';
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+const LOCATION_TASK_NAME = 'background-location-task';
+
+// TaskManager.defineTask(LOCATION_TASK_NAME, async ({ data, error }) => {
+//   if (error) {
+//     console.error('Location task error:', error);
+//     return;
+//   }
+
+//   if (data) {
+//     const locations = data;
+//     console.log('📍 Background location update:', locations);
+//   }
+// });
 
 export default function RootLayout() {
+
+  useEffect( () => {
+
+    const startBackgroundTask = async () => {
+
+      // Start by seeing if we have already set up background location stuff
+      try {
+        const alreadyStarted = await AsyncStorage.getItem('BACKGROUND_TRACKING_STARTED');
+        if (alreadyStarted) {
+          console.log('⏭️ Background location already set up.');
+          return;
+        }
+
+      // Request foreground and background permissions
+      const { status: foregroundStatus } = await Location.requestForegroundPermissionsAsync();
+      if (foregroundStatus !== 'granted') return;
+
+      // const { status: backgroundStatus } = await Location.requestBackgroundPermissionsAsync();
+      // if (backgroundStatus !== 'granted') return;
+      
+
+      // // Check if the background task is already running, if not, start it up
+      // const isRunning = await Location.hasStartedLocationUpdatesAsync(LOCATION_TASK_NAME);
+      // if (!isRunning) {
+      //     await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+      //       accuracy: Location.Accuracy.High,
+      //       timeInterval: 10000,
+      //       distanceInterval: 10,
+      //       showsBackgroundLocationIndicator: true,
+      //       pausesUpdatesAutomatically: false,
+      //     });
+        
+      //   // Update the storage so that we can know it is running
+      //   await AsyncStorage.setItem('BACKGROUND_TRACKING_STARTED', 'true');
+      //   console.log('✅ Background tracking started for the first time.');
+      //}
+    } catch(err) {
+      console.error('Failed to set up background tracking:', err);
+    }
+  };
+  
+  startBackgroundTask()
+
+  }, []);
   return(
-    <Stack screenOptions={{ headerShown: false}}>
+    <Stack screenOptions={{ headerShown: true}}>
       <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
 
--- components/Map.tsx
+++ components/Map.tsx
@@ -1,17 +1,52 @@
-import React from 'react'
-import { Platform, Text } from 'react-native'
+import { useLocation } from '@/hooks/useLocation';
+import React, { useEffect, useState } from 'react';
+import { Platform, Text } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+
+interface LatLong {
+    lat: number,
+    long: number,
+}
 
 const Map = () => {
+    const { location, errorMsg } = useLocation();
+    const [ pinCoords, setPinCoords ] = useState<LatLong>();
+
+     useEffect(() => {
+    if (location && !pinCoords) {
+      setPinCoords({
+        lat: location.coords.latitude,
+        long: location.coords.longitude,
+      });
+    }
+  }, [location]);
+
+    if (errorMsg) return <Text>Error: {errorMsg}</Text>;
+    if (!location) return <Text>Loading...</Text>;
+    
     if (Platform.OS === 'web') {
         return <Text>Map not Supported on web</Text>
     }
 
-    const MapView = require('react-native-maps').default
+    if(!pinCoords) return <Text>Loading Pin Coords</Text>
+
     return (
         <MapView style = {{ 
-            width: '100%',
-            height: '100%',
-        }}/>
+                    width: '100%',
+                    height: '100%',
+                }}
+                initialRegion={{
+                    latitude: location.coords.latitude,
+                    longitude: location.coords.longitude,
+                    latitudeDelta: 0.0322,
+                    longitudeDelta: 0.0221,
+                }}>
+
+            <Marker draggable
+                coordinate={{latitude: pinCoords.lat, longitude: pinCoords.long}}
+                onDragEnd={(e) => setPinCoords({ lat: e.nativeEvent.coordinate.latitude, long: e.nativeEvent.coordinate.longitude})}
+            />
+        </MapView>
     )
 }
 
